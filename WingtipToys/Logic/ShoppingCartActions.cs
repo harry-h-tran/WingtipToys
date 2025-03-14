27468df68263f7c836b924297b394b64a0f57486
Harry Tran <harrytran.w@gmail.com>
2025-03-13 23:54:08 -0700

    Add project files.

---
+++ WingtipToys/Logic/ShoppingCartActions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WingtipToys.Models;
+
+namespace WingtipToys.Logic
+{
+	public class ShoppingCartActions : IDisposable
+	{
+		public string ShoppingCartId { get; set; }
+
+		private ProductContext _db = new ProductContext();
+
+		public const string CartSessionKey = "CartId";
+
+		public void AddToCart(int id)
+		{
+			// Retrieve Product from db
+			ShoppingCartId = GetCartId();
+
+			var cartItem = _db.ShoppingCartItems.SingleOrDefault(
+				c => c.CartId == ShoppingCartId
+				&& c.ProductId == id);
+
+			if (cartItem == null)
+			{
+
+			}
+		}
+	}
+}
